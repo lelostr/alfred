54c1aecdc59677a987a88eb8cbe4498d23c0871a
Weslley Santos <weslley@siimp.com.br>
2025-09-27 14:36:25 +0000

    rotas de autenticacao;

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 /* Rotas protegidas por Sanctum */
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
